60da506b88d73449833ac36862ec6dfb1498f117
Eashan Adhikarla <aeashan@gmail.com>
2021-08-26 11:41:41 -0400

    yolov5 (all)

---
+++ yolov5/weights/get_pretrained_weights.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# Latest models are available here:
+# --> https://github.com/ultralytics/yolov5/releases
+
+python - <<EOF
+from utils.google_utils import attempt_download
+
+for x in ['s', 'm', 'l', 'x']:
+    attempt_download(f'yolov5{x}.pt')
+
+EOF
