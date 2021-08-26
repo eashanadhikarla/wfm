32bd6689c08a21991781e515cd7ab04d692f8d22
Eashan Adhikarla <aeashan@gmail.com>
2021-08-26 11:30:55 -0400

    yolov3 (all)

---
+++ yolov3/weights/get_pretrained_weights.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# Latest models are available here:
+# --> https://github.com/ultralytics/yolov3/releases
+
+python - <<EOF
+from utils.google_utils import attempt_download
+
+for x in ['yolov3', 'yolov3-spp', 'yolov3-tiny']:
+    attempt_download(f'{x}.pt')
+
+EOF
